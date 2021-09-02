1032a4144cb54759c8c5cc61cb39bec950204980
Greg Werner <werner.greg@gmail.com>
2021-09-02 19:35:51 -0400

    feat: Update nbgrader_config with timeout settings (#19)

--- umich-notebook/global_nbgrader_config.py
+++ umich-notebook/global_nbgrader_config.py
@@ -3,8 +3,12 @@ from nbgrader.auth import JupyterHubAuthPlugin
 
 c = get_config()
 
+c.Application.log_level = 30
+
+c.Authenticator.plugin_class = JupyterHubAuthPlugin
 
 c.Exchange.path_includes_course = True
 c.Exchange.root = "/srv/nbgrader/exchange"
-c.Authenticator.plugin_class = JupyterHubAuthPlugin
-c.Application.log_level = 30
+
+c.ExecutePreprocessor.iopub_timeout=180
+c.ExecutePreprocessor.timeout=360
